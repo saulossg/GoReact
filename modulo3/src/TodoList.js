3a15fa0299e68b5f616f12acbe81cc1986d83081
Saulo <saulossg@gmail.com>
2019-01-21 21:15:12 -0200

    First commit

---
+++ modulo3/src/TodoList.js
@@ -0,0 +1,30 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as TodoActions from './store/actions/todos'
+
+const TodoList = ({ todos, addTodo }) =>  (
+    <Fragment>
+    <ul>{todos.map(todo => <li key={todo.id}>{todo.text}</li>)}</ul>
+    <button onClick={() => addTodo('Novo todo')}>Adicionar</button>
+    </Fragment>
+);
+
+TodoList.propTypes = {
+    addTodo: PropTypes.func.isRequired,
+    todos: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number,
+        text: PropTypes.string,
+    })).isRequired,
+};
+
+const mapStateToProps = state => ({  
+    todos: state.todos,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(TodoActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
+
