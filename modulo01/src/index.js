874fc5fa0f0a742da63d13f2ffa163f0a57014d6
Saulo <saulossg@gmail.com>
2019-02-16 23:18:42 -0300

    Propriedades / Children

--- modulo01/src/index.js
+++ modulo01/src/index.js
@@ -1,1 +1,29 @@
-alert('RocketSeat');
+import React, { Component, Fragment } from "react";
+import { render } from "react-dom";
+
+class Button extends Component {
+  render() {
+    return (
+      <a href="" onClick={this.props.onClick}>
+        {this.props.children}
+      </a>
+    );
+  }
+}
+
+class App extends React.Component {
+  handleClick() {
+    alert("Botão clicado...");
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello Rockeseat</h1>
+        <Button onClick={this.handleClick}>Enviar</Button>
+      </Fragment>
+    );
+  }
+}
+
+render(<App />, document.getElementById("app"));
