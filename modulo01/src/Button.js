9ae4ec2d72a8d4b2f94239bdb4464fe4b1c21774
Saulo <saulossg@gmail.com>
2019-02-19 01:33:41 -0300

    Statefull e Stateless

---
+++ modulo01/src/Button.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export default class Button extends Component {
+  static defaultProps = {
+    children: "Salvar"
+  };
+
+  static propTypes = {
+    onClick: PropTypes.func.isRequired,
+    children: PropTypes.string
+  };
+
+  render() {
+    return <button onClick={this.props.onClick}>{this.props.children}</button>;
+  }
+}
