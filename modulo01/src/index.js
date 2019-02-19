460ea7c45ef54e3bb6c35039c71e5ae4f9b4127f
Saulo <saulossg@gmail.com>
2019-02-19 00:55:49 -0300

    setState assíncrono

--- modulo01/src/index.js
+++ modulo01/src/index.js
@@ -13,29 +13,72 @@ class Button extends Component {
   };
 
   render() {
-    return (
-      <a href="" onClick={this.props.onClick}>
-        {this.props.children}
-      </a>
-    );
+    return <button onClick={this.props.onClick}>{this.props.children}</button>;
   }
 }
 
+Button.defaultProps = {
+  children: "Teste"
+};
+
 class App extends React.Component {
+  state = {
+    counter: 0
+  };
+
   handleClick() {
     alert("Botão clicado...");
   }
+
+  handleClick2 = () => {
+    // this.setState({ counter: this.state.counter + 1 }, () => {
+    //   console.log(this.state.counter);
+    // });
+
+    // this.setState(state => ({
+    //   counter: state.counter + 1
+    // }));
+    // this.setState(state => ({
+    //   counter: state.counter + 1
+    // }));
+
+    this.setState = { counter: this.state.counter + 1 };
+    this.setState = { counter: this.state.counter + 1 };
+  };
+  handleClick2 = () => {
+    // this.setState({ counter: this.state.counter + 1 }, () => {
+    //   console.log(this.state.counter);
+    // });
+
+    //Esse state não é o mesmo state do componente e
+    //sim o state da fila.
+
+    this.setState(state => ({
+      counter: state.counter + 1
+    }));
+    this.setState(state => ({
+      counter: state.counter + 1
+    }));
+
+    // this.setState({ counter: this.state.counter + 1 });
+    // this.setState({ counter: this.state.counter + 1 });
+  };
 
   render() {
     return (
       <Fragment>
         <h1>Hello Rockeseat</h1>
-        <Button
+        {/* <Button
           onClick={() => {
             alert("Button 1");
           }}
         />
+        <br />
         <Button onClick={this.handleClick}>Enviar</Button>
+        <br /> */}
+        <Button onClick={this.handleClick2}>Somar</Button>
+        <br />
+        <h2>{this.state.counter}</h2>
       </Fragment>
     );
   }
