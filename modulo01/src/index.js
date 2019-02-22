e56f57f855877e1cfc99d5912bb87e6d322cb65e
Saulo <saulossg@gmail.com>
2019-02-21 22:23:24 -0300

    Estilos com SASS no ReactJS

--- modulo01/src/index.js
+++ modulo01/src/index.js
@@ -3,6 +3,8 @@ import React, { Component, Fragment } from "react";
 import { render } from "react-dom";
 import Button from "./Button";
 
+import "./style.scss";
+
 Button.defaultProps = {
   children: "Teste"
 };
@@ -12,74 +14,16 @@ class App extends React.Component {
     counter: 0
   };
 
-  componenDidMount() {
-    console.log("Inicializando o componente");
+  handleClick = () => {
     this.setState({ counter: this.state.counter + 1 });
-  }
-
-  componentWillMount() {
-    console.log("Destruindo o componente");
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    return nextState.counter <= 10;
-  }
-
-  componentDidUpdate(prevProps, prevState) {}
-
-  handleClick() {
-    alert("Botão clicado...");
-  }
-
-  handleClick2 = () => {
-    // this.setState({ counter: this.state.counter + 1 }, () => {
-    //   console.log(this.state.counter);
-    // });
-
-    // this.setState(state => ({
-    //   counter: state.counter + 1
-    // }));
-    // this.setState(state => ({
-    //   counter: state.counter + 1
-    // }));
-
-    this.setState = { counter: this.state.counter + 1 };
-    this.setState = { counter: this.state.counter + 1 };
-  };
-  handleClick2 = () => {
-    // this.setState({ counter: this.state.counter + 1 }, () => {
-    //   console.log(this.state.counter);
-    // });
-
-    //Esse state não é o mesmo state do componente e
-    //sim o state da fila.
-
-    this.setState(state => ({
-      counter: state.counter + 1
-    }));
-    this.setState(state => ({
-      counter: state.counter + 1
-    }));
-
-    // this.setState({ counter: this.state.counter + 1 });
-    // this.setState({ counter: this.state.counter + 1 });
   };
 
   render() {
     return (
       <Fragment>
         <h1>Hello Rockeseat</h1>
-        {/* <Button
-          onClick={() => {
-            alert("Button 1");
-          }}
-        />
-        <br />
-        <Button onClick={this.handleClick}>Enviar</Button>
-        <br /> */}
-        <Button onClick={this.handleClick2}>Somar</Button>
-        <br />
-        <h2>{this.state.counter}</h2>
+        <h2 style={{ color: "#f00" }}>{this.state.counter}</h2>
+        <Button onClick={this.handleClick}>Somar</Button>
       </Fragment>
     );
   }
